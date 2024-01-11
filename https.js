46d8384e72bb4f4212cd454ddf6a099cd4cccc41
Dionisio <iderlanoliveira19@gmail.com>
2024-01-10 21:08:15 -0300

    atualização 1.0

--- https.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-http.createServer(function(req, res){
-    res.end("Olá mundo");
-}).listen(9191);
-
-console.log("O servidor estah rodando!");
-
--- script2.js
+++ script2.js
@@ -1,9 +1,27 @@
 function adicionarItem() {
-    var newItem = document.getElementById('itemInput').value;
+    const newItem = document.getElementById('itemInput').value;
     document.getElementById('itemInput').value = '';
 
-    var tableBody = document.getElementById('tabelaBody');
-    var newRow = tableBody.insertRow(tableBody.rows.length);
-    var cell = newRow.insertCell(0);
+    const tableBody = document.getElementById('tabelaBody');
+    const newRow = tableBody.insertRow(tableBody.rows.length);
+    const cell = newRow.insertCell(0);
     cell.innerHTML = newItem;
 }
+
+document.getElementById("notificarButton").addEventListener("click", function(){
+
+if("Notification" in window){
+
+    Notification.requestPermission().then(function(Permission){
+        if(Permission === "granted"){
+
+            const notification = new Notification("SEU PEDIDO FOI SOLICITADO COM SUCESSO!", {
+                body: "Parabéns, sua solicitação foi efetuada com sucesso, agora é só esperar que o almoxarifado aceite ou rejeite sua solicitação."
+            })
+        }
+    })
+} else{
+    alert("Desculpe, houve algum erro com sua solicitação, consulte o setor de almoxarifado.")
+}
+
+})
